df3a1b8e59a12c84f4e0b602c0a0e3d27f54e024
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-12-04 17:52:28 +0530

    UC4 added

--- DemoLinkedList.java
+++ DemoLinkedList.java
@@ -1,18 +1,23 @@
 public class DemoLinkedList {
-    Node head;
+    static Node head;
     Node tail;
 
-public void append(int data) {
+    public DemoLinkedList() {
+        this.head = null;
+    }
+
+    public void append(int data) {
     Node newNode = new Node(data);
   if (head == null){
-      head = tail = newNode;
+      head =  newNode;
   }
   else{
       Node temp=head;
-      while (temp.next!=null){
+      while (temp.next!=null && temp.next.data<data){
 
           temp=temp.next;
       }
+      newNode.next=temp.next;
       temp.next=newNode;
   }
 }
--- Main.java
+++ Main.java
@@ -3,8 +3,17 @@ public class Main {
     public static void main(String[] args) {
         DemoLinkedList Demo = new DemoLinkedList();
         Demo.append(56);
-        Demo.append(30);
         Demo.append(70);
+
+
+
+        Node temp = DemoLinkedList.head;
+        while (temp.data != 56) {
+            temp = temp.next;
+        }
+        Node newNode = new Node(30);
+        newNode.next = temp.next;
+        temp.next = newNode;
         Demo.Display();
     }
 }
